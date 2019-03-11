44dff72b0f0be5db74e70ff320db08aca4a2d814
gdm-1819-thomvand26 <thomvand26@student.arteveldehs.be>
2019-03-11 18:23:56 +0100

    Update index.py

--- tinder/index.py
+++ tinder/index.py
@@ -33,7 +33,7 @@ while True:
     try:
         # sense.stick.direction_right = like
         # sense.stick.direction_left = dislike
-        sense.show_message('${title} ${first_name} ${last_name}')
+        sense.show_message('$title $first_name $last_name')
         # sense.show_message(str(type(jsonn)))
 
     except KeyboardInterrupt:
