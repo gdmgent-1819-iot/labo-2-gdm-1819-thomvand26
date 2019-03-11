9da23caafddb64ee179743586b7b28becca0cee9
gdm-1819-thomvand26 <thomvand26@student.arteveldehs.be>
2019-03-11 17:29:59 +0100

    Update index.py

--- tinder/index.py
+++ tinder/index.py
@@ -24,7 +24,7 @@ while True:
     try:
         # sense.stick.direction_right = like
         # sense.stick.direction_left = dislike
-        sense.show_message(namen)
+        sense.show_message(data)
         
     except KeyboardInterrupt:
         sense.clear()
